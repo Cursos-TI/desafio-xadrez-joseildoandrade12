cf43299b9bb7834ec2e53bfa684b95d290ab1d11
Joseildo Andrade <140559913+joseildoandrade12@users.noreply.github.com>
2025-08-20 22:47:00 +0000

    movimentação cavalo loop

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int torre = 0, bispo = 0, rainha = 0;
+    int torre = 0, bispo = 0, rainha;
     while (torre < 5) {
         torre++;
         printf("Torre movimentou: Direita\n");
@@ -15,6 +15,15 @@ int main() {
         bispo++;
         printf("Bispo movimentou: Cima, Direita\n");
     } while (bispo < 5);
-    
+
+    printf("\n");
+
+    for (int i = 0; i < 2; i++) {
+        printf("Cavalo movimentou: Baixo\n");
+        for (int j = 0; j < i; j++) {
+            printf("Cavalo movimentou: Esquerda\n");
+        }
+    }
+
     return 0;
 }
