53b9747e9988718e385c97b30640d46ac491e99e
Joseildo Andrade <140559913+joseildoandrade12@users.noreply.github.com>
2025-08-21 22:39:17 +0000

    refatoração

--- xadrez.c
+++ xadrez.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 
-int main() {
-    int torre = 0, bispo = 0, rainha;
-    while (torre < 5) {
-        torre++;
+void moverTorre(int casas) {
+    if (casas > 0) {
         printf("Torre movimentou: Direita\n");
+        moverTorre(casas - 1);
     }
+}
 
-    for (rainha = 0; rainha < 8; rainha++) {
+void moverRainha(int casas) {
+    if (casas > 0) {
         printf("Rainha movimentou: Esquerda\n");
+        moverRainha(casas - 1);
     }
+}
 
-    do {
-        bispo++;
-        printf("Bispo movimentou: Cima, Direita\n");
-    } while (bispo < 5);
+void moverBispo(int casas) {
+    if (casas > 0) {
+        printf("Bispo movimentou: Cima\n");
+        printf("Bispo movimentou: Direita\n");
+        moverBispo(casas - 1);
+    }
+}
 
-    printf("\n");
-
+void moverCavalo() {
     for (int i = 0; i < 2; i++) {
         printf("Cavalo movimentou: Baixo\n");
         for (int j = 0; j < i; j++) {
             printf("Cavalo movimentou: Esquerda\n");
         }
     }
+}
 
+int main()
+{
+    moverTorre(5);
+    moverRainha(8);
+    moverBispo(2);
+    printf("\n");
+    moverCavalo();
     return 0;
 }
